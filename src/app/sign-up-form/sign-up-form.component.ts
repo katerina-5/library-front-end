c2546d1d09a65a947d6671ff3552e24d56615f6a
katerina-5 <pochta13524@gmail.com>
2019-03-12 00:20:48 +0200

    Add sign up form component

---
+++ src/app/sign-up-form/sign-up-form.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up-form',
+  templateUrl: './sign-up-form.component.html',
+  styleUrls: ['./sign-up-form.component.css']
+  // styleUrls: ['./../sign-in-form/sign-in-form.component.css']
+})
+export class SignUpFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  register() { }
+
+}
