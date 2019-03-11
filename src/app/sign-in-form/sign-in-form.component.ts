d81daba4d35835271b228ffee4d8612d6e92a735
katerina-5 <pochta13524@gmail.com>
2019-03-12 00:20:02 +0200

    Add sign in form component

---
+++ src/app/sign-in-form/sign-in-form.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+// import { MatCardModule } from '@angular/material/card';
+// import { MatFormFieldModule, MatInputModule, MatToolbarModule, MatIconModule, MatListModule } from '@angular/material';
+
+@Component({
+  selector: 'app-sign-in-form',
+  templateUrl: './sign-in-form.component.html',
+  styleUrls: ['./sign-in-form.component.css']
+})
+export class SignInFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  auth() { }
+
+}
