b888b54a7f7374eb9d4fb2bbcee8e77435aee952
katerina-5 <pochta13524@gmail.com>
2019-03-29 19:37:21 +0200

    Add settings for Angular Material and modify styles for all forms

--- src/app/components/pages/sign-in-form/sign-in-form.component.ts
+++ src/app/components/pages/sign-in-form/sign-in-form.component.ts
@@ -9,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInFormComponent implements OnInit {
 
+  hide = true;
+
   constructor() { }
 
   ngOnInit() {
--- src/app/components/pages/sign-up-form/sign-up-form.component.ts
+++ src/app/components/pages/sign-up-form/sign-up-form.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpFormComponent implements OnInit {
 
+  hide = true;
+
   constructor() { }
 
   ngOnInit() {
