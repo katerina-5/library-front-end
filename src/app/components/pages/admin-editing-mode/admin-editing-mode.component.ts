228d4b38dbe096389a9fcd9586c337dc6420d7e3
katerina-5 <pochta13524@gmail.com>
2019-05-01 15:56:58 +0300

    Add method for parseBook by url

--- src/app/components/pages/admin-editing-mode/admin-editing-mode.component.ts
+++ src/app/components/pages/admin-editing-mode/admin-editing-mode.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/shared/services/api.service';
 
 @Component({
   selector: 'app-admin-editing-mode',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminEditingModeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataSource: ApiService) { }
 
   ngOnInit() {
+  }
+
+  pressEnter(event) {
+    let url = '';
+    if (event.key === 'Enter') {
+      url = event.target.value;
+      this.parseBook(url);
+    }
+  }
+
+  parseBook(url) {
+    this.dataSource.parseBook(url)
+      .subscribe((data: any) => {
+        console.log(data);
+      });
   }
 
 }
