9a4141e4550927efb651a03ce6ec32422080adf6
katerina-5 <pochta13524@gmail.com>
2019-05-03 00:00:42 +0300

    Update search in books-catalog

--- src/app/components/pages/books-catalog/books-catalog.component.ts
+++ src/app/components/pages/books-catalog/books-catalog.component.ts
@@ -145,20 +145,35 @@ export class BooksCatalogComponent implements OnInit {
   }
 
   search(searchRequest) {
-    let message = '';
-    message += 'Search button was pressed\n';
-    message += 'Search request: ' + searchRequest;
-    alert(message);
+    if (searchRequest === '') {
+      this.dataSource.getBooks()
+        .subscribe((data: any) => {
+          console.log(data);
+          this.books = data;
+        });
+
+      return;
+    }
+    // let message = '';
+    // message += 'Search button was pressed\n';
+    // message += 'Search request: ' + searchRequest;
+    // alert(message);
+    this.dataSource.searchBooks(searchRequest)
+      .subscribe((data: any) => {
+        console.log(data);
+        this.books = data;
+      })
   }
 
   pressEnter(event) {
-    let message = '';
-    if (event.key === 'Enter') {
-      // this.search(event.target.value);
-      message += 'Enter button was pressed\n';
-      message += 'Search request: ' + event.target.value;
-      alert(message);
-    }
+    // let message = '';
+    // if (event.key === 'Enter') {
+    //   // this.search(event.target.value);
+    //   message += 'Enter button was pressed\n';
+    //   message += 'Search request: ' + event.target.value;
+    //   alert(message);
+    // }
+    this.search(event.target.value);
   }
 
 }
